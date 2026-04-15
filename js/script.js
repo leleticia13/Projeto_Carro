3c7bced803a70c40a427e1d8d8744cc192c9ab05
flaletsil <leticiaflaviasilva0@gmail.com>
2026-04-14 21:13:08 -0300

    captura dos dados do formulário

--- js/script.js
+++ js/script.js
@@ -4,3 +4,10 @@ const lista = document.getElementById("listaVeiculos");
 form.addEventListener("submit", function (e) {
     e.preventDefault();
 });
+
+let modelo = document.getElementById("modelo").value;
+let marca = document.getElementById("marca").value;
+let placa = document.getElementById("placa").value;
+let ano = document.getElementById("ano").value;
+let valor = document.getElementById("valor").value;
+let combustivel = document.querySelector('input[name="combustivel"]:checked');
