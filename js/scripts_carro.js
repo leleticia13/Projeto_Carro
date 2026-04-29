5b8a006b731c0d944aa9ab8d3867131069626565
flaletsil <leticiaflaviasilva0@gmail.com>
2026-04-29 11:07:46 -0300

    Novas alterações

--- js/scripts_carro.js
+++ js/scripts_carro.js
@@ -5,21 +5,21 @@ const btnCalcular = document.querySelector('#calcular');
 const resultado = document.querySelector('#card-veiculo');
 
 /* Array pessoa */
-const formulário = [];
+const Formulário = [];
 
 /* capturando o evento de click no botão de calcular */
 btnCalcular.addEventListener('click', async (e) => {
     e.preventDefault();
 
-    const formformulário = new FormData(formImc);
-    const objformulário = {
-        modelo: formformulário.get('modelo'),
-        marca: formformulário.get('marca'),
-        placa: formformulário.get('placa'),
-        anoDeFabricacao: parseFloat(formformulário.get('anoDeFabricacao')),
-        valorDoVeiculo: parseFloat(formformulário.get('valorDoVeiculo')),
-        cor: formformulário.get('cor'),
-        tipoCombustivel: formformulário.get('tipoCombustivel')
+    const formformulario = new FormData(formVeiculo);
+    const objformulario = {
+        modelo: formformulario.get('modelo'),
+        marca: formformulario.get('marca'),
+        placa: formformulario.get('placa'),
+        anoDeFabricacao: formformulario.get('anoDeFabricacao'),
+        valorDoVeiculo: formformulario.get('valorDoVeiculo'),
+        cor: formformulario.get('cor'),
+        tipoCombustivel: formformulario.get('tipoCombustivel')
     }
     const resultadoSalvar = await salvarArquivo(objformulário);
     formImc.reset();
